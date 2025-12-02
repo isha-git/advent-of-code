ee1cda7752dffe0c462e9df4575e1367a30608cc
iagrawal1 <isha.agrawal@vodafoneziggo.com>
2025-12-02 20:00:12 +0100

    solved day 2 part 2

--- scripts/day_2/2.py
+++ scripts/day_2/2.py
@@ -1,33 +1,41 @@
 class GiftShop:
     def __init__(self):
         self.input_path = "input/day_2.txt"
-        self.invalid_count = 0
+        self.invalid_total = 0
+
+    def possible_invalid_length(self, number_length):
+        possible_lengths = []
+        for i in range(1, int(number_length)):
+            if number_length % i == 0:
+                possible_lengths.append(i)
+
+        return possible_lengths
 
     def find_invalid(self, left: str, right: str):
-        left_length = len(left)
-        right_length = len(right)
+        invalid_values = set()
 
-        if left_length % 2 != 0:
-            left = "1" + "0" * left_length
-            if int(left) > int(right):
-                return 0
+        chunk_sizes = [len(left)]
+        curr_num = left
+        while True:
+            chunk_size = len(curr_num) + 1
+            if chunk_size > len(right):
+                break
+            else:
+                chunk_sizes.append(chunk_size)
+            curr_num = "1" + curr_num
 
-        if right_length % 2 != 0:
-            right = "1" + "0" * (right_length - 1)
-            if int(right) < int(left):
-                return 0
+        for chunk_size in chunk_sizes:
+            possible_lengths = self.possible_invalid_length(chunk_size)
+            for pattern_length in possible_lengths:
+                upper_limit = int("9" * pattern_length)
+                for i in range(1, upper_limit + 1):
+                    new_num = int(str(i) * (chunk_size // pattern_length))
+                    if new_num > int(right):
+                        break
+                    if (new_num >= int(left)) and (new_num <= int(right)):
+                        invalid_values.add(new_num)
 
-        left_length = len(left)
-        right_length = len(right)
-
-        left_left_chunk = left[: int(left_length / 2)]
-        diff = int((int(right) - int(left)) // 2)
-
-        for i in range(diff):
-            new_num = int(left_left_chunk * 2)
-            if (new_num >= int(left)) and (new_num <= int(right)):
-                self.invalid_count += new_num
-            left_left_chunk = str(int(left_left_chunk) + 1)
+        return invalid_values
 
     def read_input(self) -> list:
         with open(self.input_path, "r") as f:
@@ -40,9 +48,12 @@ class GiftShop:
         ids = self.read_input()
         for id in ids:
             left, right = id.split("-")
-            self.find_invalid(left, right)
+            invalid_values = self.find_invalid(left, right)
 
-        print(self.invalid_count)
+            if invalid_values:
+                for val in invalid_values:
+                    self.invalid_total += int(val)
+        print("Invalid total:", self.invalid_total)
 
 
 GiftShop().find_total()
